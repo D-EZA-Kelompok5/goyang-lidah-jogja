f6479b064f11d4a25053fe002bcf4d7128045a8b
Malvin <malvinmraqin@gmail.com>
2024-10-24 10:20:55 +0700

    add navbar, login, and register modals

--- main/views.py
+++ main/views.py
@@ -7,46 +7,39 @@ import datetime
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 
-@login_required(login_url='/login')
+# @login_required(login_url='/login')
 def show_main(request):
     context = {
         'class': 'PBP D',
         'group': '5',
-        'last_login': request.COOKIES['last_login'],
+        # 'last_login': request.COOKIES.get('last_login'),  # Use .get to avoid KeyError
     }
-
     return render(request, "main.html", context)
 
 def register_user(request):
     form = UserCreationForm()
-
     if request.method == "POST":
         form = UserCreationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, 'Your account has been successfully created!')
-            return redirect('main:login')
-    context = {'form':form}
+            return redirect('main:show_main')  # Correct namespace
+    context = {'form': form}
     return render(request, 'register.html', context)
 
 def login_user(request):
-    if request.method == 'POST':
-        form = AuthenticationForm(data=request.POST)
+    form = AuthenticationForm(request, data=request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        user = form.get_user()
+        login(request, user)
+        response = HttpResponseRedirect(reverse("main:show_main"))  # Ensure 'main:show_main' is correct
+        response.set_cookie('last_login', str(datetime.datetime.now()))
+        return response
 
-        if form.is_valid():
-            user = form.get_user()
-            login(request, user)
-            response = HttpResponseRedirect(reverse("main:show_main"))
-            response.set_cookie('last_login', str(datetime.datetime.now()))
-            return response
-
-    else:
-        form = AuthenticationForm(request)
-    context = {'form': form}
-    return render(request, 'login.html', context)
+    return render(request, 'base.html', {'form': form})
 
 def logout_user(request):
     logout(request)
-    response = HttpResponseRedirect(reverse('main:login'))
+    response = HttpResponseRedirect(reverse('main:show_main'))  # Correct namespace
     response.delete_cookie('last_login')
     return response
