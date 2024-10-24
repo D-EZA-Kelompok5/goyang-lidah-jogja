b88a20f6af48610f149ff9efc7ed57fe02eeb463
AlyssaLayla <alyssasasti@gmail.com>
2024-10-24 14:19:42 +0700

    Role pada User - Belum models.py

--- main/urls.py
+++ main/urls.py
@@ -6,7 +6,7 @@ app_name = 'main'
 
 urlpatterns = [
     path('', views.show_main, name='main'),  # Landing page
-    path('login/', views.login_user, name='login'),  # Login
+    path('login/', views.login_user, name='login_user'),  # Login
     path('register/', views.register_user, name='register'),  # Register
     path('logout/', views.logout_user, name='logout'),  # Logout
     path('show_main/', views.show_main, name='show_main'),
