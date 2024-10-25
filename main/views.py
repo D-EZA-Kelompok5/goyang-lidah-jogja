b5e7f41b4fc1c0157c4cc4046cb7f312bd20c3d0
Malvin <malvinmraqin@gmail.com>
2024-10-26 00:44:53 +0700

    update design menu sementara dan module ulasGoyangan

--- main/views.py
+++ main/views.py
@@ -13,6 +13,7 @@ from django.contrib.auth import get_user_model
 from managerDashboard.models import Event
 from django.db.models import Avg
 from ulasGoyangan.models import Review  # Import Review from ulasGoyangan
+from django.db.models import Avg, Count
 
 
 # @login_required(login_url='/login')
@@ -73,21 +74,36 @@ from ulasGoyangan.models import Review
 
 def menu_detail(request, menu_id):
     menu = get_object_or_404(Menu, id=menu_id)
-    
-    # Calculate average rating
+
+    # Get all reviews for the menu
     reviews = Review.objects.filter(menu=menu)
+
+    # Calculate average rating
     if reviews.exists():
         average_rating = reviews.aggregate(Avg('rating'))['rating__avg']
+        average_rating = round(average_rating, 1)
     else:
         average_rating = 0  # Default to 0 if no reviews
+
+    # Calculate rating distribution
+    total_reviews = reviews.count()
+    rating_counts = reviews.values('rating').annotate(count=Count('rating'))
+    rating_distribution = {'5': 0, '4': 0, '3': 0, '2': 0, '1': 0}
+
+    for item in rating_counts:
+        rating = str(item['rating'])
+        count = item['count']
+        percentage = (count / total_reviews) * 100
+        rating_distribution[rating] = round(percentage, 2)
 
     # Pass a fixed range for stars
     star_range = [1, 2, 3, 4, 5]
 
     context = {
         'menu': menu,
-        'average_rating': round(average_rating, 1),
+        'average_rating': average_rating,
         'star_range': star_range,
+        'rating_distribution': rating_distribution,  # Add this line
     }
     return render(request, 'menu_detail.html', context)
 
