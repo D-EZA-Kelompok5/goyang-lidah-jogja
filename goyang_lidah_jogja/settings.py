de837e11f717f45607dfbe0a5b7ff512022f3511
rakabimar <rakabimaghaniendra22@gmail.com>
2024-10-27 14:01:24 +0700

    Update settings and models

--- goyang_lidah_jogja/settings.py
+++ goyang_lidah_jogja/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'managerDashboard',
     'ulasGoyangan',  
     'userPreferences',
+    'menuResto'
 ]
 
 
--- userPreferences/models.py
+++ userPreferences/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tag(models.Model):
     name = models.CharField(max_length=50, unique=True)
-    menus = models.ManyToManyField('main.Menu', related_name='tags')
+    menus = models.ManyToManyField('menuResto.Menu', related_name='tags')
 
     def __str__(self):
         return self.name
