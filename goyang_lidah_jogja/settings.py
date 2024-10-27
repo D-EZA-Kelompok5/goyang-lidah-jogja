d6affbcd3dd6677d32a1fcc7dc1e75e1ca4c7a6f
Malvin <malvinmraqin@gmail.com>
2024-10-27 17:11:38 +0700

    reponsive navbar and menu_detail and my apps, add lv

--- goyang_lidah_jogja/settings.py
+++ goyang_lidah_jogja/settings.py
@@ -72,6 +72,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'ulasGoyangan.context_processors.review_count',
             ],
         },
     },
